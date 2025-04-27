a86d5ffc58f0a082c8cdaaed6bb3ca51d16ce1aa
James <6036965+jtadamec@users.noreply.github.com>
2025-04-26 20:18:02 -0700

    Implement IDictionary

--- src/Adamechanics.Collections/ReplacementDictionary.cs
+++ src/Adamechanics.Collections/ReplacementDictionary.cs
@@ -24,9 +24,8 @@ namespace Adamechanics.Collections
 
             Opening = opening;
             Closing = closing;
-        }
-
-        public string this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            Dictionary = new Dictionary<string, string>();
+        }   
 
         /// <summary>
         /// The opening character sequence for the keys contained in this instance
@@ -39,6 +38,11 @@ namespace Adamechanics.Collections
         public string Closing { get; }
 
         /// <summary>
+        /// The underlying dictionary
+        /// </summary>
+        private Dictionary<string,string> Dictionary { get; }
+
+        /// <summary>
         /// Formats the provided value into a key suitable for use in the calling instance
         /// </summary>
         /// <param name="raw">The raw value to be made into a replacement key</param>
@@ -46,68 +50,87 @@ namespace Adamechanics.Collections
 
         #region IDictionary
 
-        public ICollection<string> Keys => throw new NotImplementedException();
+        public int Count => Dictionary.Count;
 
-        public ICollection<string> Values => throw new NotImplementedException();
+        public bool IsReadOnly => false;
 
-        public int Count => throw new NotImplementedException();
-
-        public bool IsReadOnly => throw new NotImplementedException();
+        public string this[string key] 
+        {
+            get
+            {
+                ArgumentException.ThrowIfNullOrWhiteSpace(key);
+                return Dictionary[MakeKey(key)];
+            }
+            set
+            {
+                ArgumentException.ThrowIfNullOrWhiteSpace(key);
+                Dictionary[MakeKey(key)] = value;
+            }
+        }
 
         public void Add(string key, string value)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Add(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
+            Dictionary.Add(MakeKey(key), value);
         }
 
         public bool ContainsKey(string key)
         {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
+            return Dictionary.ContainsKey(MakeKey(key));
         }
 
         public bool Remove(string key)
         {
-            throw new NotImplementedException();
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
+            return Dictionary.Remove(MakeKey(key));
         }
 
-        public bool Remove(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
+        public void Clear() => Dictionary.Clear();
 
         public bool TryGetValue(string key, [MaybeNullWhen(false)] out string value)
         {
-            throw new NotImplementedException();
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
+            return Dictionary.TryGetValue(MakeKey(key), out value);
+        }
+
+        #region Explicit Interface Implementations 
+
+        void ICollection<KeyValuePair<string, string>>.Add(KeyValuePair<string, string> item)
+        {
+            ((ICollection<KeyValuePair<string, string>>)Dictionary).Add(item);
+        }
+
+        bool ICollection<KeyValuePair<string, string>>.Contains(KeyValuePair<string, string> item)
+        {
+            return ((ICollection<KeyValuePair<string, string>>)Dictionary).Contains(item);
+        }
+
+        void ICollection<KeyValuePair<string, string>>.CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            ((ICollection<KeyValuePair<string, string>>)Dictionary).CopyTo(array, arrayIndex);
+        }
+
+        bool ICollection<KeyValuePair<string, string>>.Remove(KeyValuePair<string, string> item)
+        {
+            return ((ICollection<KeyValuePair<string, string>>)Dictionary).Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, string>>)Dictionary).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return ((IEnumerable)Dictionary).GetEnumerator();
         }
+
+        ICollection<string> IDictionary<string,string>.Keys => Dictionary.Keys;
+
+        ICollection<string> IDictionary<string,string>.Values => Dictionary.Values;
+
+        #endregion
 
         #endregion
     }
